8a0ed0025fb136bc86e29e172908aaf68cdc6c43
KIDOKY <118824913+KIDOKY@users.noreply.github.com>
2024-08-06 11:31:26 +0900

    Add files via upload
    첫번째 예제

---
+++ Hello.java
@@ -0,0 +1,21 @@
+package day01;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*여러줄 주석8*/
+		/**
+		 * 작성일:
+		 * 작성자:
+		 * 내용:
+		 */
+		for(int i=1; i < 11 ; i++) {
+		System.out.println("Hello Java");
+		}
+		
+		System.out.print("Hello Java!");
+		System.out.print("Hello Java2!");
+	}
+
+}
