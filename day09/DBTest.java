f72837248033aa14680b4e28ee56acc2d09b6a94
KIDOKY <118824913+KIDOKY@users.noreply.github.com>
2024-08-19 16:42:49 +0900

    Add files via upload

---
+++ day09/DBTest.java
@@ -0,0 +1,56 @@
+package day09;
+/*
+ * 	1. JDDBC 드라이버 로드
+ *  2. 데이터 베이스 연결
+ */
+
+import java.sql.*;
+
+public class DBTest {
+
+	public static void main(String[] args) {
+		Connection con = null;
+		
+		try {
+			//1. 드라이버 로딩...
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			//2. 데이터 베이스 연결...
+			String url = "jdbc:mysql://localhost:3306/contacts";
+			String id = "root";
+			String pwd = "1234";
+			con = DriverManager.getConnection(url, id, pwd);
+			System.out.println("DB 연결 성공~~");
+			
+			// 3. sql 실행 하기 위한 Statement, PreparedStatement 객체생성
+			//String sql = "INSERT INTO person VALUES ('kido','010-456-4566','kim@a.com',30);";
+			String sql2 = "insert into person (name, phone, email, age) " + "values (?,?,?,?);";
+			PreparedStatement st = con.prepareStatement(sql2);
+			st.setString(1, "고길동");
+			st.setString(2, "010-783-7896");
+			st.setString(3, "lee@a.com");
+			st.setInt(4, 20);
+			
+			// 4. 데이터 추가 명령 전송하기
+			int result = st.executeUpdate();
+			
+			if(result > 0) {
+				System.out.println("데이터 저장 성공~~");
+			}else {
+				System.out.println("데이터 저장 실패!!");
+			}
+			
+		}catch(Exception e) {
+			System.out.println("드라이버 로딩 실패");
+			
+		}finally {
+			try {
+				con.close();
+				System.out.println("연결 끊기");
+			}catch(SQLException es) {
+				
+			}
+		}
+	}
+
+}
